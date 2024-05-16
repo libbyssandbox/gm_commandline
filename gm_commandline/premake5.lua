8694c9ac76210f232779e72d62fc18213de1bd80
Zach <147861361+1054009@users.noreply.github.com>
2024-05-15 23:52:52 -0400

    Add SDK stuff to script

--- gm_commandline/premake5.lua
+++ gm_commandline/premake5.lua
@@ -3,6 +3,9 @@ PROJECT_GENERATOR_VERSION = 3
 include("./garrysmod_common/")
 CreateWorkspace({name = "commandline", abi_compatible = false, path = "projects/"})
 	CreateProject({serverside = true, source_path = "src", manual_files = false})
+		IncludeHelpersExtended()
+		IncludeSDKCommon()
+		IncludeSDKTier0()
 		files({
 			"src/**.*"
 		})
